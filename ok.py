c3ddf11465060a2df829b60204e25fbc4d19d7a9
Mohammad Raj <122721728+S4BB1R-69@users.noreply.github.com>
2023-04-04 00:00:54 +0600

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,8 +1,1 @@
-import os, sys
-os.system("git pull")
-os.system("pip uninstall requests chardet urllib3 idna certifi -y && pip install chardet urllib3 idna certifi requests")
-os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
-try:
-    __import__("RAJ.so").menu()
-except Exception as e:
-    exit(str(e))
+import RAJ.so
